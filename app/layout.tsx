ad93867ec1bd4c5d144028230842cda8dd5b27f2
Alpha <alphakilolambda@gmail.com>
2025-12-05 22:48:37 +0300

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="virtual-protocol-site-verification"
+          content="806be1859a42c3170083bb517b661f02"
+        />
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   )
